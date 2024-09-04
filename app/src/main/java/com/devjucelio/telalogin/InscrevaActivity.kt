d80a3dbccc87cf829436d9c8a9346e921074adf5
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2024-09-04 09:57:19 -0300

    Tela 2/3 finalizada.

---
+++ app/src/main/java/com/devjucelio/telalogin/InscrevaActivity.kt
@@ -0,0 +1,20 @@
+package com.devjucelio.telalogin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class InscrevaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_inscreva)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
